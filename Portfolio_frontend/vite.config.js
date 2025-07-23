117ca7e2c21ced705efdba56fcb0b188ddb901f3
Kartik Jhamb <241230026@nitdelhi.ac.inm>
2025-07-23 16:44:01 +0530

    D

--- Portfolio_frontend/vite.config.js
+++ Portfolio_frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(),react()],
   server: {
     host: '0.0.0.0',  // This makes the server accessible on local network
     port: 5173,       // Optional: can be 3000 or any other
