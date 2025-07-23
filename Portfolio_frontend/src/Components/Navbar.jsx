976ac129d61c3ff675ea2e092a46262657fd46f5
Kartik Jhamb <241230026@nitdelhi.ac.inm>
2025-07-23 22:55:39 +0530

    o

--- Portfolio_frontend/src/Components/Navbar.jsx
+++ Portfolio_frontend/src/Components/Navbar.jsx
@@ -27,7 +27,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
 
         {/* Logo */}
-        <div className="text-2xl font-extrabold text-black tracking-tight">
+        <div className="text-2xl font-extrabold font-cursive text-black tracking-tight">
           Kartik Jhamb
         </div>
 
